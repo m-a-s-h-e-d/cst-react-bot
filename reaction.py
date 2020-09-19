dd63327a072c11bdbb62241b556898c174592fc1
notMashed <53192198+notMashed@users.noreply.github.com>
2020-09-18 20:24:34 -0700

    Hidden token and removed extra

--- reaction.py
+++ reaction.py
@@ -56,5 +56,4 @@ async def on_raw_reaction_remove(payload):
         else:
             print("Role not found")
 
-#client.run(os.environ['DISCORD_TOKEN'])
-client.run('NzU2MzAyNTMxMTUxMzk2OTg2.X2P3ng.vXoDwrCeXJexEsVpdn0lfZ5zMrw')
+client.run(os.environ['DISCORD_TOKEN'])
