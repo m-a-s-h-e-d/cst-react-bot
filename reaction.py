a15468dbed5e2242240b63069288ecd4f0cd590a
notMashed <53192198+notMashed@users.noreply.github.com>
2020-09-22 18:08:09 -0700

    Added more roles

--- reaction.py
+++ reaction.py
@@ -36,11 +36,41 @@ async def on_raw_reaction_add(payload):
                 print("Member not found")
         else:
             print("Role not found")
+    if message_id == 758132060467494922:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+        role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+        if role is not None:
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            if member is not None:
+                await member.add_roles(role)
+                print("Assigned")
+            else:
+                print("Member not found")
+        else:
+            print("Role not found")
 
 @client.event
 async def on_raw_reaction_remove(payload):
     message_id = payload.message_id
     if message_id == 756312146803163198:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+        role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+        if role is not None:
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            if member is not None:
+                await member.remove_roles(role)
+                print("Removed")
+            else:
+                print("Member not found")
+        else:
+            print("Role not found")
+    if message_id == 758132060467494922:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
 
