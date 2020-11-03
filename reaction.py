462d7acf3e3ad34a88d537efc5f74d6d11c65382
notMashed <53192198+notMashed@users.noreply.github.com>
2020-11-02 16:27:23 -0800

    Updated with sets

--- reaction.py
+++ reaction.py
@@ -24,26 +24,24 @@ async def on_ready():
 @client.event
 async def on_raw_reaction_add(payload):
     message_id = payload.message_id
-    if message_id == 756312146803163198:
+    if message_id == 756312146803163198 or message_id == 758132060467494922 or message_id == 772976347592392716:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
 
-        role = discord.utils.get(guild.roles, name=payload.emoji.name)
-
-        if role is not None:
-            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.add_roles(role)
-                print("Assigned")
-            else:
-                print("Member not found")
+        if payload.emoji.name == 'regional_indicator_a':
+            role = discord.utils.get(guild.roles, name='a')
+        elif payload.emoji.name == 'regional_indicator_b':
+            role = discord.utils.get(guild.roles, name='b')
+        elif payload.emoji.name == 'regional_indicator_c':
+            role = discord.utils.get(guild.roles, name='c')
+        elif payload.emoji.name == 'regional_indicator_d':
+            role = discord.utils.get(guild.roles, name='d')
+        elif payload.emoji.name == 'regional_indicator_e':
+            role = discord.utils.get(guild.roles, name='e')
+        elif payload.emoji.name == 'regional_indicator_f':
+            role = discord.utils.get(guild.roles, name='f')
         else:
-            print("Role not found")
-    if message_id == 758132060467494922:
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-
-        role = discord.utils.get(guild.roles, name=payload.emoji.name)
+            role = discord.utils.get(guild.roles, name=payload.emoji.name)
 
         if role is not None:
             member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
@@ -58,26 +56,24 @@ async def on_raw_reaction_add(payload):
 @client.event
 async def on_raw_reaction_remove(payload):
     message_id = payload.message_id
-    if message_id == 756312146803163198:
+    if message_id == 756312146803163198 or message_id == 758132060467494922 or message_id == 772976347592392716:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
 
-        role = discord.utils.get(guild.roles, name=payload.emoji.name)
-
-        if role is not None:
-            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.remove_roles(role)
-                print("Removed")
-            else:
-                print("Member not found")
+        if payload.emoji.name == 'regional_indicator_a':
+            role = discord.utils.get(guild.roles, name='a')
+        elif payload.emoji.name == 'regional_indicator_b':
+            role = discord.utils.get(guild.roles, name='b')
+        elif payload.emoji.name == 'regional_indicator_c':
+            role = discord.utils.get(guild.roles, name='c')
+        elif payload.emoji.name == 'regional_indicator_d':
+            role = discord.utils.get(guild.roles, name='d')
+        elif payload.emoji.name == 'regional_indicator_e':
+            role = discord.utils.get(guild.roles, name='e')
+        elif payload.emoji.name == 'regional_indicator_f':
+            role = discord.utils.get(guild.roles, name='f')
         else:
-            print("Role not found")
-    if message_id == 758132060467494922:
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-
-        role = discord.utils.get(guild.roles, name=payload.emoji.name)
+            role = discord.utils.get(guild.roles, name=payload.emoji.name)
 
         if role is not None:
             member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
